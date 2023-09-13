7e67ae640efbbe1113cabd85625072f9bbbf76a1
Volodymyr <shabaldasengr@gmail.com>
2023-09-14 01:46:25 +0300

    Prompt_B (feat): display spinner when searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,13 +2,6 @@
 
 class SearchController < ApplicationController
   def search
-    @results = Prompt.search(params[:query], highlight: { tag: '<strong style="background-color: yellow">' }
-    )
-    respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.update('results', partial: 'home/search_results',
-                                                            locals: { results: @results })
-      end
-    end
+    @results = Prompt.search(params[:query], highlight: { tag: '<strong style="background-color: yellow">' })
   end
 end
