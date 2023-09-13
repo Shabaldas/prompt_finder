9a0d81dc2d42031f6fa2c819a1cc46ee8454eb79
Volodymyr <shabaldasengr@gmail.com>
2023-09-13 19:40:57 +0300

    Prompt_F (feat): add js styling for better search UI

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import DisablingController from "./disabling_controller"
+application.register("disabling", DisablingController)
