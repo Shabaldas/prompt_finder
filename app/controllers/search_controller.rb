28c99bed33181f5e9953453429b5e7929a8a5958
Volodymyr <shabaldasengr@gmail.com>
2023-09-14 04:31:13 +0300

    (fix): pagy preview

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,28 +2,7 @@
 
 class SearchController < ApplicationController
   def search
-    @results = Prompt.search(params[:query], highlight: { tag: '<strong style="background-color: yellow">' })
-    # collection = Prompt.pagy_search(params[:query], highlight: { tag: '<strong>' })
-    # @pagy, @results = pagy_searchkick(collection, items: 10)
-  end
-
-  def sanitize_string_for_elasticsearch_string_query(str)
-    # Escape special characters
-    # http://lucene.apache.org/core/old_versioned_docs/versions/2_9_1/queryparsersyntax.html#Escaping Special Characters
-    escaped_characters = Regexp.escape('\\/+-&|!(){}[]^~*?:')
-    str = str.gsub(/([#{escaped_characters}])/, '\\\\\1')
-  
-    # AND, OR and NOT are used by lucene as logical operators. We need
-    # to escape them
-    ['AND', 'OR', 'NOT'].each do |word|
-      escaped_word = word.split('').map {|char| "\\#{char}" }.join('')
-      str = str.gsub(/\s*\b(#{word.upcase})\b\s*/, " #{escaped_word} ")
-    end
-  
-    # Escape odd quotes
-    quote_count = str.count '"'
-    str = str.gsub(/(.*)"(.*)/, '\1\"\3') if quote_count % 2 == 1
-  
-    str
+    collection = Prompt.pagy_search(params[:query], highlight: { tag: '<strong style="background-color: yellow">' })
+    @pagy, @results = pagy_searchkick(collection, items: 10)
   end
 end
